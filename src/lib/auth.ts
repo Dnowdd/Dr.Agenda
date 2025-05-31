15778ae81cc5e868cae3d050dcd22144b7ac4c46
David Queiroz <david.queiroz@dnowdd.com>
2025-05-31 20:34:41 -0300

    feat: add Stripe webhook handling and update user schema for subscription management

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -5,6 +5,7 @@ import { eq } from "drizzle-orm";
 
 import { db } from "@/db";
 import * as schema from "@/db/schema";
+import { usersToClinicsTable } from "@/db/schema";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
@@ -20,21 +21,23 @@ export const auth = betterAuth({
   },
   plugins: [
     customSession(async ({ user, session }) => {
-      // TO-DO: Ao adaptar para o user ter multiplas clinicas, alterar isso aqui
-      const [clinic] = await db.query.usersToClinicsTable.findMany({
-        where: eq(schema.usersToClinicsTable.userId, user.id),
+      const clinics = await db.query.usersToClinicsTable.findMany({
+        where: eq(usersToClinicsTable.userId, user.id),
         with: {
           clinic: true,
+          user: true,
         },
       });
-
+      // TODO: Ao adaptar para o usuário ter múltiplas clínicas, deve-se mudar esse código
+      const clinic = clinics?.[0];
       return {
         user: {
           ...user,
+          plan: clinic?.user.plan,
           clinic: clinic?.clinicId
             ? {
                 id: clinic?.clinicId,
-                name: clinic?.clinic.name,
+                name: clinic?.clinic?.name,
               }
             : undefined,
         },
@@ -44,6 +47,23 @@ export const auth = betterAuth({
   ],
   user: {
     modelName: "usersTable",
+    additionalFields: {
+      stripeCustomerId: {
+        type: "string",
+        fieldName: "stripeCustomerId",
+        required: false,
+      },
+      stripeSubscriptionId: {
+        type: "string",
+        fieldName: "stripeSubscriptionId",
+        required: false,
+      },
+      plan: {
+        type: "string",
+        fieldName: "plan",
+        required: false,
+      },
+    },
   },
   session: {
     modelName: "sessionsTable",
